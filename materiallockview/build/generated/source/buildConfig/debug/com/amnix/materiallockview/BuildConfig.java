a19b92c3bb3260c3e5dc28167ec056f2cd77af58
guipasse <ktekesis@hotmail.com>
2017-06-11 22:57:08 +0200

    Add files via upload

---
+++ materiallockview/build/generated/source/buildConfig/debug/com/amnix/materiallockview/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.amnix.materiallockview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.amnix.materiallockview";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
